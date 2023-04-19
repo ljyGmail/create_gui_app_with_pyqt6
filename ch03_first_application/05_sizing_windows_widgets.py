9ec1d47322998607ff49558fda87021ba01ce468
ljy <liangjy881@gmail.com>
2023-04-19 11:07:51 +0900

    ch03: first application

---
+++ ch03_first_application/05_sizing_windows_widgets.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('My App')
+
+        button = QPushButton('Press Me!')
+
+        self.setFixedSize(QSize(400, 300))
+
+        # Set the central widget of the window
+        self.setCentralWidget(button)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
